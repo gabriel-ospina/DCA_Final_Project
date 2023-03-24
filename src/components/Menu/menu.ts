c6a538bd7e5ac1812ce70457c9ff78a76973de74
gabriel-ospina <foxgdo@hotmail.com>
2023-03-23 22:24:19 -0500

    Feat: added menu component

---
+++ src/components/Menu/menu.ts
@@ -0,0 +1,62 @@
+export enum MenuAttribute{
+    "mainicon"="mainicon",
+    "chaticon"="chaticon",
+    "safeiconin"="safeiconin",
+    "safeiconout"="safeiconout"
+}
+
+class Menu extends HTMLElement{
+    mainicon?: string;
+    chaticon?: string;
+    safeiconin?: string;
+    safeiconout?: string;
+
+    static get observedAttributes(){
+        const menuattrs: Record<MenuAttribute, null> = {
+            mainicon: null,
+            chaticon: null,
+            safeiconin: null,
+            safeiconout: null,
+        }
+        return Object.keys(menuattrs);
+    }
+
+    constructor(){
+        super();
+        this.attachShadow({ mode: "open"});
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    attributeChangedCallback(
+        propName: MenuAttribute,
+        _: string | undefined,
+        newValue: string | undefined
+    ){
+        this[propName]=newValue;
+        this.render();
+    }
+
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <nav>
+                <div class="main-selec">
+                    <svg width="17" height="22" viewBox="0 0 17 22"><path d="${this.mainicon}" fill="#816CAD"></path></svg>
+                    <span>Main</span>
+                </div>
+                <svg viewBox="0 0 27 27" width="27" heigth="27"><path d="${this.chaticon}" fill="black" fill-opacity="0.36"></path></svg>
+                <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="${this.safeiconin}" fill="black" fill-opacity="0.36"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="${this.safeiconout}" fill="black" fill-opacity="0.36"/>
+                </svg>
+            </nav>
+            `;
+        }
+    }
+}
+
+customElements.define("menu-nav", Menu);
+export default Menu
